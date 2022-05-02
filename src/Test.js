ed0727afe779ca3ef0ea2d56ddba7d1c61ac45f9
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-05-02 12:51:27 +0800

    New

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+function Test() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, facilis?
+        Sit, aliquid aliquam. Fuga, quod nihil fugiat quo placeat distinctio
+        enim eos sunt error dolorem deserunt ullam labore nam voluptatibus.
+      </p>
+    </div>
+  );
+}
+
+export default Test;
