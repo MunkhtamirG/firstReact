86afb413b3d59f53e31d635b9a5b6f363dd35f4c
mstars_lab3_03 <mstars_lab3_03@mstars-lab3-03s-iMac.local>
2022-05-10 13:23:52 +0800

    CLEAR

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Dog from "./Dog";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dog />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
